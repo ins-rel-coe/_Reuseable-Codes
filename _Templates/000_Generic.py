9fb5c3958306c216272c3b0c6b962cad0c592fbc
ins-rel-coe <45580382+ins-rel-coe@users.noreply.github.com>
2018-12-05 09:04:31 +0800

    Create 000_Generic.py

---
+++ _Templates/000_Generic.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import time
+
+# Time start
+t_start = time.time()
+
+##############################START CODE###############################
+
+
+
+###############################END CODE################################
+
+# Time end
+t_end = time.time()
+print("\n\nExecution completed in "
+      + "{0:.2f}".format(t_end-t_start)
+	  + " second(s).\n\n")
